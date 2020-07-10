d06368f3abca28c53e93ef8fbbf70af0755be146
Todd Yamakawa <todd.yamakawa@arm.com>
2020-07-10 13:04:34 -0500

    new-window: Adding zsh-nnn.

--- scripts/bin/new-window-menu.bash
+++ scripts/bin/new-window-menu.bash
@@ -3,10 +3,19 @@ declare -r CURRENT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 # TODO: Check support for `display-menu`
 # TODO: Figure out how to align bottom left
 # TODO: Format borders
+
+# Options
+options=()
+options+=('current directory' 'c' "run-shell '$CURRENT_DIR/new-window.bash'")
+if (which zsh-nnn &>/dev/null); then
+	options+=('nnn' 'n' "new-window 'zsh-nnn'")
+fi
+if (which xclip &>/dev/null); then
+	options+=('from clipboard' 'p' "run-shell '$CURRENT_DIR/new-window.bash $(xclip -o)'")
+fi
+options+=('temp directory'    't' "run-shell '$CURRENT_DIR/new-window.bash $(mktemp -d)'")
+
 exec tmux display-menu -y S \
 	-T '#[align=centre bold]New Window' \
-	'current directory' 'c' "run-shell '$CURRENT_DIR/new-window.bash'" \
-	'temp directory'    't' "run-shell '$CURRENT_DIR/new-window.bash $(mktemp -d)'" \
-	'from clipboard'    'p' "run-shell '$CURRENT_DIR/new-window.bash $(xclip -o)'" \
-	'quit' 'q' ''
+	"${options[@]}"
 
