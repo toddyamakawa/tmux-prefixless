41fdc11b8e7815ec28aea841b5498cca9dbc5727
Todd Yamakawa <todd.yamakawa@arm.com>
2021-05-10 11:04:29 -0500

    smooth-scroll: Adding steps.

--- scripts/bin/smooth-scroll.bash
+++ scripts/bin/smooth-scroll.bash
@@ -2,6 +2,7 @@
 
 direction=${1?direction missing}
 n=${2-1}
+step=5
 if [[ "$n" == 'half' ]]; then
 	n="$(tmux display-message -p '#{pane_height}')"
 	n=$((n/2))
@@ -11,7 +12,12 @@ elif [[ "$n" == 'full' ]]; then
 fi
 
 while (( n > 0 )); do
-	tmux send-keys -X cursor-$direction
-	n=$((n-1))
+	if ((step > n)); then
+		tmux send-keys -X -N $n cursor-$direction
+		n=0
+	else
+		tmux send-keys -X -N $step cursor-$direction
+		n=$((n-step))
+	fi
 done
 
